cc6f1fa0cb92deae5c20685e1734195176a07ee9
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-10-30 01:15:53 +0530

    Add Country to database done

--- brand/models.py
+++ brand/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    country_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.country_name)

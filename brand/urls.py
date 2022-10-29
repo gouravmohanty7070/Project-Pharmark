ad43991f2db433aa1e8b84a02acab668ad5de38a
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-10-30 01:22:15 +0530

    templating system for homepage and predict page init

--- brand/urls.py
+++ brand/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import homepage, predictFormPage
 
 urlpatterns = [
-
+    path('', homepage, name='home'),
+    path('predictForm/', predictFormPage, name='predict_form')
 ]

ce719e7ba84b78a4a7b1904f37ac25f3c731fbd9
goyalpramod <goyalpramod1729@gmail.com>
2023-01-04 11:42:03 +0530

    change models to include default india

--- brand/models.py
+++ brand/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
-    country_name = models.CharField(max_length=100)
+    country_name = models.CharField(max_length=100, default="india")
 
     def __str__(self) -> str:
         return str(self.country_name)
